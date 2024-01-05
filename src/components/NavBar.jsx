5f0d807a9b79f4a722bcf38d1f1306c6ba675bb8
unknown <38173610+NYCJames@users.noreply.github.com>
2024-01-04 21:21:04 -0500

    Add basic React Router DOM functionality\

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { TbShoppingCart } from "react-icons/tb";
+import { Link, Outlet } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <>
+      <div className="navbar">
+        <div className="links">
+          <Link to="/">Home</Link>
+          <Link to="/cart">
+            <TbShoppingCart size={32} />
+          </Link>
+        </div>
+      </div>
+
+      <Outlet></Outlet>
+    </>
+  );
+}
+
+export default NavBar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import NavBar from "./components/NavBar.jsx";
+import Shop from "./pages/Shop.jsx";
+import ShoppingCart from "./pages/ShoppingCart.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<NavBar></NavBar>}>
+        <Route index element={<Shop></Shop>}></Route>
+        <Route path="cart" element={<ShoppingCart></ShoppingCart>}></Route>
+      </Route>
+    </>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
