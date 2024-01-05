3c0fa628adf6354a1952c0c7372074ee5cf4201b
unknown <38173610+NYCJames@users.noreply.github.com>
2024-01-05 15:09:57 -0500

    Randomized Product IDs

--- src/Products.js
+++ src/Products.js
@@ -19,49 +19,49 @@ product1 =
 
 export const PRODUCTS = [
   {
-    id: 1,
+    id: 8546631,
     productName: "IPhone",
     price: 999.0,
     productImage: product1,
   },
   {
-    id: 2,
+    id: 64161456,
     productName: "Macbook Pro 2022 (M1)",
     price: 1999.0,
     productImage: product2,
   },
   {
-    id: 3,
+    id: 6451,
     productName: "Cannon M50 Camera",
     price: 699.0,
     productImage: product3,
   },
   {
-    id: 4,
+    id: 16451,
     productName: "WLS Van Gogh Denim Jacket",
     price: 228.0,
     productImage: product4,
   },
   {
-    id: 5,
+    id: 41685,
     productName: "LED Light Strips",
     price: 19.99,
     productImage: product5,
   },
   {
-    id: 6,
+    id: 61526,
     productName: "SPECTRUM LS TEE",
     price: 68.0,
     productImage: product6,
   },
   {
-    id: 7,
+    id: 967815,
     productName: "AUTO SERVICE SHIRT by GOLF WANG",
     price: 120.0,
     productImage: product7,
   },
   {
-    id: 8,
+    id: 487,
     productName: "DON'T TRIP UNSTRUCTURED HAT",
     price: 40.0,
     productImage: product8,
